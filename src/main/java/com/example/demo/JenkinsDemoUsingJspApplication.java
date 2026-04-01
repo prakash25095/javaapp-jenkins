d09d97752ed26f682d4c00961f30ff01245f000a
Prakash Rane <prakashrane2509@gmail.com>
2026-04-01 16:09:55 +0530

    extended by servletinitializer

--- src/main/java/com/example/demo/JenkinsDemoUsingJspApplication.java
+++ src/main/java/com/example/demo/JenkinsDemoUsingJspApplication.java
@@ -2,9 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsDemoUsingJspApplication{
+public class JenkinsDemoUsingJspApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsDemoUsingJspApplication.class, args);
